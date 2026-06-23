4bebabaaf9a77641ad5ccded2e61d1d1a6450efb
Danny <179722984+DanielProano@users.noreply.github.com>
2026-06-22 21:22:55 -0400

    FPU & NVIC

--- examples/blinky/main.c
+++ examples/blinky/main.c
@@ -1,5 +1,7 @@
 #include <rcc.h>
 #include <gpio.h>
+#include <fpu.h>
+#include <nvic.h>
 #include "stdint.h"
 
 void delay(volatile uint32_t count) {
@@ -7,9 +9,11 @@ void delay(volatile uint32_t count) {
 }
 
 int main(void) {
+    fpu_init();
     rcc_init();
     rcc_enable_gpioc();
     gpio_init_pc13();
+    nvic_init();
 
     while (1) {
         gpio_toggle_pc13();
